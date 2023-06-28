b6ead2d9220934528cc08289b1a04f027b0e30bc
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-06-28 16:57:34 -0300

    Tela de Login e Redirecionamento

---
+++ assets/JS/index.js
@@ -0,0 +1,15 @@
+if (localStorage.getItem("token") == null){
+    alert("Faça login para acessar essa página!");
+    window.location.href="../assets/html/sigin.html";
+}
+
+let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+
+let logado = document.querySelector(".Logado");
+logado.innerHTML = 'Olá ${userLogado.nome}';
+
+function sair() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLogado");
+    window.location.href="../html/sigin.html";
+}
