bbd4835e77c7aace9713bac8c812b304cfbcc5de
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-06-29 12:52:15 -0300

    Window restaurado

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem("token") == null){
     alert("Faça login para acessar essa página!");
-    document.location.href="../assets/html/sigin.html";
+    window.location.href="../assets/html/sigin.html";
 }
 
 let userLogado = JSON.parse(localStorage.getItem("userLogado"));
@@ -11,7 +11,7 @@ logado.innerHTML = 'Olá ${userLogado.nome}';
 function sair() {
     localStorage.removeItem("token");
     localStorage.removeItem("userLogado");
-    document.location.href="../html/sigin.html";
+    window.location.href="../html/sigin.html";
 }
 
 //testando nova branch de projeto compartilhado
