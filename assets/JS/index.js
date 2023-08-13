9177df9984dc86f1417be01b2df7773705d7ed00
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-08-13 20:44:07 -0300

    fix: :bug: Site 100% finalizado

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,17 +1,15 @@
 if (localStorage.getItem("token") == null){
     alert("Faça login para acessar essa página!");
-    window.location.href="../assets/html/sigin.html";
+    window.location.href="./assets/HTML/signin.html";
 }
 
 let userLogado = JSON.parse(localStorage.getItem("userLogado"));
 
-let logado = document.querySelector("#Logado");
-logado.innerHTML = 'Olá ${userLogado.nome}';
+let logado = document.querySelector("#logado");
+logado.innerHTML = `Ola ${userLogado.nome}`;
 
 function sair() {
     localStorage.removeItem("token");
     localStorage.removeItem("userLogado");
-    window.location.href="../assets/html/sigin.html";
+    window.location.href="./assets/HTML/signin.html";
 }
-
-//testando nova branch de projeto compartilhado
